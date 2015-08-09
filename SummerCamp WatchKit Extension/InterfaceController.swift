adf5a25592eb38fe9b06340858133698453675bc
nikHowlett <nikhowlett@gmail.com>
2015-08-09 10:12:42 -0400

    Starbucks1

--- SummerCamp WatchKit Extension/InterfaceController.swift
+++ SummerCamp WatchKit Extension/InterfaceController.swift
@@ -14,22 +14,27 @@ import CoreData
 class InterfaceController: WKInterfaceController {
 
     var psilocybin: [NSArray] = [NSArray]()
+    var titles: [String] = [String]()
+    var titlesCount = 0
+    var activitesCount = 0
     var defaults = NSUserDefaults(suiteName: "group.UCBAuth")
     
     @IBOutlet weak var number: WKInterfaceLabel!
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        var defaults = NSUserDefaults(suiteName: "group.UCBAuth")
-        defaults?.synchronize()
-        loadActivites()
-        var activitescount = psilocybin.count
-        if activitescount == 0 {
-            print("try again")
+        //go to the correct interface based on the closest activity's title
+        if titlesCount == 0 {
+        //update the interface to go to the screen that says no more activites, a congratulations screen
+        //maybe this needs a self.
+        presentControllerWithName(“NoMore”,
+          context: [“segue”: “pagebased”,
+                “data”: “Passed through page-based navigation”])
         } else {
-            number.description == "\(activitescount)"
-            print(activitescount)
-            //number.
+        //go to the correct interface based on the closest activity's title
+        var titleString = titlesCount[0]
+        //associate the activity.id with the \(i) variable
+        
         }
     }
     
@@ -38,26 +43,48 @@ class InterfaceController: WKInterfaceController {
         print("dataisbeensent", appendNewline: false)
         WKInterfaceController.openParentApplication(dict, reply: {(reply, error) -> Void in print("Data has been sent to target: parent iOS app - UCB Pharma", appendNewline: false)
         })
-        
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        
+        psilocybin = []
+        titles = []
+        var defaults = NSUserDefaults(suiteName: "group.UCBAuth")
+        defaults?.synchronize()
+        loadActivites()
+        var activitescount = psilocybin.count
+        if activitescount == 0 {
+            print("try again")
+        } else {
+            number.setText("\(activitescount)")
+            print(activitescount)
+        }
+        self.setTitle("1 / \(activitescount)")
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
     
     func loadActivites() {
+        var charlie = false;
         for (var i = 0; i < 275; i++) {
-            var thisstring = "Activity \(i)"
+            charlie = false
+            var thisActivityTitle = "Activity \(i)"
             var thethingusaved = NSUserDefaults.standardUserDefaults().objectForKey(thisstring) as! NSArray
             psilocybin.append(thethingusaved)
+            //find out how "thethingusaved" variable looks when empty or not
+            if thethingusaved !=nil {
+                charlie = true
+            }
+            if charlie {
+                titles.append(thisActivityTitle)
+            }
         }
+        //create an array of the titles of "Activity \(i) that you send
+        //in another for loop, loop throught the titles, and pull them
+        titlesCount = titles.count
+        activitesCount = psilocybin.count
     }
     
     
@@ -65,14 +92,14 @@ class InterfaceController: WKInterfaceController {
 //types
 //emojis, activity, yes-no, slider
 
-/*var instagramHooks = "window.open('yammer://threads')"
-var instagramUrl = NSURL(string: instagramHooks)
-if UIApplication.sharedApplication().canOpenURL(instagramUrl!)
+/*var yammerHooks = "window.open('yammer://threads')"
+var yammerUrl = NSURL(string: yammerHooks)
+if UIApplication.sharedApplication().canOpenURL(yammerUrl!)
 {
-UIApplication.sharedApplication().openURL(instagramUrl!)
+UIApplication.sharedApplication().openURL(yammerUrl!)
 
 } else {
-//redirect to safari because the user doesn't have Instagram
+//redirect to safari because the user doesn't have Yammer
 println("App not installed")
 UIApplication.sharedApplication().openURL(NSURL(string: "https://itunes.apple.com/in/app/instagram/id389801252?m")!)
 
