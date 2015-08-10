48c1262a8d027dfa3f4ab286f7a41a568f2fa019
nikHowlett <nikhowlett@gmail.com>
2015-08-09 22:52:17 -0400

    starbucks2

--- SummerCamp WatchKit Extension/InterfaceController.swift
+++ SummerCamp WatchKit Extension/InterfaceController.swift
@@ -10,32 +10,57 @@ import WatchKit
 import Foundation
 import CoreData
 
-
 class InterfaceController: WKInterfaceController {
-
+    //make this the file for the slider and just have that be the first option i guess
     var psilocybin: [NSArray] = [NSArray]()
     var titles: [String] = [String]()
+    var activityNumbers: [Int] = [Int]()
     var titlesCount = 0
     var activitesCount = 0
     var defaults = NSUserDefaults(suiteName: "group.UCBAuth")
+    var thisPageType = "slider"
     
     @IBOutlet weak var number: WKInterfaceLabel!
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         //go to the correct interface based on the closest activity's title
+        openNextPage()
+    }
+    
+    func openNextPage() {
+        var thisType = ""
         if titlesCount == 0 {
-        //update the interface to go to the screen that says no more activites, a congratulations screen
-        //maybe this needs a self.
-        presentControllerWithName(“NoMore”,
-          context: [“segue”: “pagebased”,
-                “data”: “Passed through page-based navigation”])
+            //update the interface to go to the screen that says no more activites, a congratulations screen
+            //maybe this needs a self.
+            pushControllerWithName(“NoMore”, context: [“segue”: “hierarchical”,
+             “data”:“Passed through hierarchical navigation”])
         } else {
-        //go to the correct interface based on the closest activity's title
-        var titleString = titlesCount[0]
-        //associate the activity.id with the \(i) variable
-        
+            //go to the correct interface based on the closest activity's title
+            var thisActivityID = activityTitles[0]
+            //associate the activity.id with the \(i) variable
+            //go through psilocybin and pull activity.type
+            //find the index where activity.id = thisActivityID
+            //then pull activity
+            for (var p = 0; p < psilocybin.count-1; p++) {
+                var thisActObj = psilocybin[p]
+                var thisID = thisActObj.id
+                if thisID == thisActivityID {
+                    //get the type
+                    thisType = thisActObj.type
+                }
+            }
+            if thisType != thisPageType {
+               loadThisPage()
+            } else {
+                pushControllerWithName(“\(thisType)”, context: [“segue”: “hierarchical”,
+             “data”:“Passed through hierarchical navigation”])
+            }
         }
+        //ok so this is watch nav so its different than phone nav
+        //instead of passing the activity object to the next page like usual,
+        //we're gonna load the activites the same way we did on this page, but its gonna be the right page, each page will have a type
+        
     }
     
     @IBAction func Open() {
@@ -49,6 +74,7 @@ class InterfaceController: WKInterfaceController {
         super.willActivate()
         psilocybin = []
         titles = []
+        activityTitles = []
         var defaults = NSUserDefaults(suiteName: "group.UCBAuth")
         defaults?.synchronize()
         loadActivites()
@@ -79,6 +105,7 @@ class InterfaceController: WKInterfaceController {
             }
             if charlie {
                 titles.append(thisActivityTitle)
+                activityNumbers.append(i)
             }
         }
         //create an array of the titles of "Activity \(i) that you send
